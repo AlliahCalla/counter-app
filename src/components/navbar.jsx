3a8796c73d1ff813b0cdd186a4935dd2f0d157c9
Alliah <alliahczarielle17@gmail.com>
2021-02-02 14:54:23 +1300

    Life Cycle hooks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,18 @@
-const NavBar = ({totalCounters}) => {
-    return (
-        <nav className="navbar navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar{" "}
-              <span className="badge badge-pill badge-secondary">
-                {totalCounters}
-              </span>
-            </a>
-          </div>
-        </nav>
-      );
-    
+import React from "react";
+const NavBar = ({ totalCounters }) => {
+  console.log("NavBar - Rendered");
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
 };
-
 
 export default NavBar;
